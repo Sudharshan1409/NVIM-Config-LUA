d88ca9b70bae980d16f50c1a6a7e5db115d9212d
Sudharshan V <sudarshan61kv@gmail.com>
2023-11-23 02:06:22 +0530

    changes

--- lua/plugin_config/oil.lua
+++ lua/plugin_config/oil.lua
@@ -50,7 +50,6 @@ require("oil").setup({
         ["<C-t>"] = "actions.select_tab",
         ["<C-p>"] = "actions.preview",
         ["<C-c>"] = "actions.close",
-        ["q"] = "actions.close",
         ["r"] = "actions.refresh",
         ["-"] = "actions.parent",
         ["_"] = "actions.open_cwd",
