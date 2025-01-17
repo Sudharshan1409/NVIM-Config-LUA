b2acc4cc38267bf973d6910cc19bae2d3aa01e3e
Sudharshan V <sudarshan61kv@gmail.com>
2025-01-17 13:28:50 +0530

    changes

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -228,21 +228,6 @@ local plugins = {
     },
     -- lazy.nvim
     {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            -- add any options here
-        },
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
-        }
-    },
-    {
         { 'codota/tabnine-nvim', build = "./dl_binaries.sh" },
     }
 }
--- lua/plugin-config/code-helpers/tabnine.lua
+++ lua/plugin-config/code-helpers/tabnine.lua
@@ -1,6 +1,6 @@
 require('tabnine').setup({
     disable_auto_comment = true,
-    accept_keymap = "<C-y>",
+    accept_keymap = "<tab>",
     dismiss_keymap = "<C-i>",
     debounce_ms = 800,
     suggestion_color = { gui = "#808080", cterm = 244 },
