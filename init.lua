8ecd5099953d206fb114fb90ed4bfa8cea49811f
Sudharshan V <sudarshan61kv@gmail.com>
2025-02-20 18:09:02 +0530

    added blame commit config

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 require("core")
 require("plugin-config.init")
+
+local project_config = vim.fn.getcwd() .. "/.nvim/project_local/init.lua"
+
+if vim.fn.filereadable(project_config) == 1 then
+	vim.cmd("source " .. project_config)
+end
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -103,20 +103,6 @@ local plugins = {
 			})
 		end,
 	},
-	{
-		"meatballs/notebook.nvim",
-		config = function()
-			require("notebook").setup()
-		end,
-	},
-	"neovim/nvim-lspconfig",
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = {
-			"nvimtools/none-ls-extras.nvim",
-		},
-	},
-	"MunifTanjim/prettier.nvim",
 	"ThePrimeagen/vim-with-me",
 	"ThePrimeagen/vim-be-good",
 	"eandrju/cellular-automaton.nvim",
@@ -191,8 +177,11 @@ local plugins = {
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
 	},
-
 	{
 		"HiPhish/nvim-ts-rainbow2",
 	},
@@ -260,6 +249,26 @@ local plugins = {
 		{ "codota/tabnine-nvim", build = "./dl_binaries.sh" },
 	},
 	{ "kosayoda/nvim-lightbulb" },
+	{
+		"f-person/git-blame.nvim",
+		-- load the plugin at startup
+		event = "VeryLazy",
+		-- Because of the keys part, you will be lazy loading this plugin.
+		-- The plugin wil only load once one of the keys is used.
+		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
+		-- or lazy = false. One of both options will work.
+		opts = {
+			-- your configuration comes here
+			-- for example
+			enabled = true, -- if you want to enable the plugin
+			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%d-%m-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+			message_when_not_committed = "Bruh!! Please commit this",
+			highlight_group = "Comment",
+			use_blame_commit_file_urls = true,
+		},
+	},
 }
 
 local opts = {}
